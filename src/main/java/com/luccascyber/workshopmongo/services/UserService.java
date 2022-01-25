22a8a394b7ce309bf49398f1a99efe85a255846b
Luccas Marcos <luccasmarcos002@gmail.com>
2022-01-25 10:17:56 -0300

    Find User by Id

--- src/main/java/com/luccascyber/workshopmongo/services/UserService.java
+++ src/main/java/com/luccascyber/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.luccascyber.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luccascyber.workshopmongo.domain.User;
+import com.luccascyber.workshopmongo.exception.ObjectNotFoundException;
 import com.luccascyber.workshopmongo.repository.UserRepository;
 
 @Service
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
